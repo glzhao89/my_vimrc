a8662c475cb16dbf537837c5ee6efcb62f836b71
Ganlin Zhao <ganlinzhao@hotmail.com>
2021-11-23 17:44:53 +0800

    fixed install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 HOME_DIR=`echo $HOME`
-VIM_DIR=$HOME_DIR/.vim
+VIM_DIR=$HOME_DIR/.vim/
 PLUGIN_DIR=$VIM_DIR/plugin
 echo $PLUGIN_DIR
 #copy .vimrc
-cp .vimrc $VIM_DIR
+cp .vimrc $HOME_DIR
 
 #copy cscope key map
 mkdir -p $PLUGIN_DIR && cp cscope_maps.vim $PLUGIN_DIR
